912d6e81de2f44c303ef16f241303d244d5f7db7
sebax774 <bugansebastian@gmail.com>
2020-10-19 11:39:02 +0200

    vypracovanie

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -5,8 +5,23 @@ namespace Open_Lab_02._02
     public class Formatter
     {
         public void Format(double[] numbers)
+
         {
-            throw new NotImplementedException();
+            string back = null;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Math.Round(numbers[i], 2, MidpointRounding.AwayFromZero);
+                back = back + numbers[i].ToString() + " ";
+
+            }
+            Console.Write(back.Replace(",", ".").TrimEnd());
         }
+
+
+
+
+
+
     }
 }
